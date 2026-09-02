42fb3e64337e0217b80b1c7b684ba2e15dd85b44
geasoesanti <24430291_gea_s@widyadharma.ac.id>
2026-09-02 19:33:21 +0700

    versi 3

--- index.php
+++ index.php
@@ -6,6 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <php echo "Hello World"; ?>
+    <?php
+    $nama = "Budi"
+    $umur = 30;
+    $sudahmenikah = false;
+
+    echo "Nama saya $nama, umur saya $umur tahun";
+
+    if ($sudahmenikah == true) {
+        echo "Sudah menikah"
+    } else {
+        echo "Belum menikah"
+    }
+
+    for ($i=1; $i < 10; $i+=2) {
+        echo "<br>";
+        echo $i;
+    }
+
+    if ($harga > 100000) {
+        $disk = $harga * 0.10;
+        $total = $harga - $disk;
+        echo "Total belanja Anda adalah $total";
+    } else {
+        echo "Total belanja Anda adalah $harga";
+    }
+    ?>
 </body>
 </html>
